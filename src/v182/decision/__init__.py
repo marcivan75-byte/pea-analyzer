60702fbbc95f2052420a9fa6e2276bd8364534b3
marcivan75-byte <marcivan75@gmail.com>
2026-08-07 17:54:06 +0200

    [audit-cycle2] Avoid eager decision module import side effects

--- src/v182/decision/__init__.py
+++ src/v182/decision/__init__.py
@@ -1,3 +1,13 @@
-from .analyst_momentum import enrich_analyst_momentum, consensus_score_100
-
 __all__ = ["enrich_analyst_momentum", "consensus_score_100"]
+
+
+def __getattr__(name):
+    if name in __all__:
+        from .analyst_momentum import enrich_analyst_momentum, consensus_score_100
+
+        values = {
+            "enrich_analyst_momentum": enrich_analyst_momentum,
+            "consensus_score_100": consensus_score_100,
+        }
+        return values[name]
+    raise AttributeError(name)
