fcbd767a35034b81c04e5964d11b62fb54367fad
marcivan75-byte <marcivan75@gmail.com>
2026-08-16 14:45:23 +0200

    route IPO V1.2 SEC source through hardened runtime

--- src/v182/sources/__init__.py
+++ src/v182/sources/__init__.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
 from . import sec_ipo_v2 as sec_ipo
+from . import sec_ipo_deep_v1_2_runtime as sec_ipo_deep_v1_2
 
-__all__ = ["sec_ipo"]
+__all__ = ["sec_ipo", "sec_ipo_deep_v1_2"]
